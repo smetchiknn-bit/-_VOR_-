0ac7e1f0fd39306246db7889f5495f964b94c6e6
SmetchikNN <smetchiknn@gmail.com>
2026-09-08 13:20:13 +0300

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+--- src/main.tsx (原始)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+
+
++++ src/main.tsx (修改后)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
